3e0e4d6c8c9ce5e90769553c2bb352dbe62288c5
william <william@lumos.lan>
2013-02-08 13:29:03 +0100

    download webcam pics for bg

--- weather/run_helpers.sh
+++ weather/run_helpers.sh
@@ -5,7 +5,12 @@ do
  echo "got current"
  wget http://api.openweathermap.org/data/2.2/forecast/city/2925177?mode=daily_compact -O forecast > /dev/null 2>&1
  echo "got forecast"
- python analogclock/clock.py
+ wget http://www.uniklinik-freiburg.de/modules/webcam/550/1.jpg -O temppic.jpg >/dev/null 2>&1
+ mv temppic.jpg 1.jpg
+ echo "got pic"
+# python analogclock/clock.py
+ #uncomment following line if using python2
+ python analogclock/clock_python2.py
  echo "set clock"
  sleep 30
 done

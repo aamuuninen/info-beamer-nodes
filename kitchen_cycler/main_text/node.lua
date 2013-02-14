b4fad2d9b3099bce3086b280443b366db2abbc02
Daniel <daniel@daniel-P300.(none)>
2013-02-14 21:17:12 +0100

    add photos

--- kitchen_cycler/main_text/node.lua
+++ kitchen_cycler/main_text/node.lua
@@ -1,9 +1,9 @@
 gl.setup(1024, 768)
 
-font = resource.load_font("silkscreen.ttf")
+font = resource.load_font("font.ttf")
 
 function node.render()
     font:write(60, 300, "Freunde der Nacht", 80, 1,0,0,1)
     font:write(60, 400, "WG Beamer", 80, 0,1,0,1)
-   sys.sleep(2000)
+   --sys.sleep(2000)
 end

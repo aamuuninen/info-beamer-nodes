d95c0b1d54b0fae55b42d65a71256005c6e526f9
Beamer User <beamer@lumos.(none)>
2013-02-25 01:21:44 +0100

    change the text in main_text node

--- kitchen_cycler/main_text/node.lua
+++ kitchen_cycler/main_text/node.lua
@@ -4,6 +4,6 @@ font = resource.load_font("font.ttf")
 
 function node.render()
     font:write(60, 300, "Freunde der Nacht", 80, 1,0,0,1)
-    font:write(60, 400, "WG Beamer", 80, 0,1,0,1)
+    font:write(60, 400, "WG Monitor", 80, 0,1,0,1)
    --sys.sleep(2000)
 end
